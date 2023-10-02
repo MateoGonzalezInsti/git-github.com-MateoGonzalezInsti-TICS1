499ac95668341d603ed746442ff1d1afdcf92943
MateoGonzalezInsti <matgonido@iesxulianmagarinos.org>
2023-10-02 11:34:30 +0200

    terminado

--- Boletin2/Ej_todos.py
+++ Boletin2/Ej_todos.py
@@ -77,4 +77,25 @@ else:
 print(f'La puntuacion del trabajador es {puntuacionTrabajadorUsuario_ej8}({textoTrabajador_ej8}) y el dinero es {puntuacionTrabajadorUsuario_ej8*2400}')
 #ej9
 edadclienteUsuario_ej9=int(input('Introduce su edad:'))
-if
+if edadclienteUsuario_ej9<4:
+    precioEntrada_ej9=str('gratis')
+elif edadclienteUsuario_ej9>=4 and edadclienteUsuario_ej9<=18:
+     precioEntrada_ej9=int(5)
+else:
+    precioEntrada_ej9=int(10)
+print(f'La entrada es {precioEntrada_ej9}')
+#ej10
+print('Pizzas:')
+vgtUsuario_ej10=int(input('1)vegetariano , 2)no vegetariana'))
+
+if vgtUsuario_ej10==1:
+    ingredientesUsuario_ej10=int(input('● Ingredientes vegetarianos: 1)Pimiento , 2)tofu.'))
+    ingredientestext_ej10=['Pimiento','tofu']
+    ingredientesUsuario_ej10=str(ingredientestext_ej10[ingredientesUsuario_ej10-1])
+    vgtUsuario_ej10=str('vegetariana')
+else:
+    ingredientesUsuario_ej10=int(input('● Ingredientes no vegetarianos: 1)Peperoni, 2)Jamón , 3)Salmón.'))
+    ingredientestext_ej10=['Peperoni','Jamón','Salmón']
+    ingredientesUsuario_ej10=str(ingredientestext_ej10[ingredientesUsuario_ej10-1])
+    vgtUsuario_ej10=str('no vegetariana')
+print(f'La pizza es {vgtUsuario_ej10}.Ingredientes: mozzarella, tomate y {ingredientesUsuario_ej10}')
