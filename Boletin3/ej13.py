932d292d678c7c51a232fa566e1c21ab290df2c2
MateoGonzalezInsti <matgonido@iesxulianmagarinos.org>
2023-10-05 13:33:21 +0200

    boletin 3 ej

--- Boletin3/ej13.py
+++ Boletin3/ej13.py
@@ -1,2 +1,10 @@
 #Escribir un programa que muestre el eco de todo lo que el usuario introduzca hasta que el usuario escriba “salir” que terminará.
 
+terminado=False
+while terminado==False:
+    palabra=str(input('Introduce una palabra (salir para terminar programa):'))
+    if palabra=='salir':
+        terminado=True
+    else:
+        print(f'Eco: {palabra}')
+print('El programa se terminó')
