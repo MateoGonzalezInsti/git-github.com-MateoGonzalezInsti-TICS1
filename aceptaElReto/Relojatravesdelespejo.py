6af3cab57eb0e4dbf0856250786d91c8f5386f26
MateoGonzalezInsti <matgonido@iesxulianmagarinos.org>
2024-01-26 11:55:46 +0100

    fin

--- aceptaElReto/Relojatravesdelespejo.py
+++ aceptaElReto/Relojatravesdelespejo.py
@@ -1,16 +1,12 @@
 import datetime
 
-entrada = str(
-    input(
-        "Introduzca una hora en formato HH:MM y que estea comprendida entre 01:00 y 12:59: "
-    )
+entrada = input(
+    "Introduzca una hora en formato HH:MM y que estea comprendida entre 01:00 y 12:59: "
 )
 hora = datetime.datetime.strptime(entrada, "%H:%M").time()
 horaEspejo = hora
-if horaEspejo.minute!=0:
-    horaEspejo = horaEspejo.replace(minute=(60-horaEspejo.minute))
-print(horaEspejo)
-if hora.minute > 0:
-   horaEspejo = horaEspejo.replace(hour=hora.hour - 1)
-
+horaEspejo = horaEspejo.replace(hour=12 - horaEspejo.hour)
+if hora.minute != 0:
+    horaEspejo = horaEspejo.replace(minute=(60 - horaEspejo.minute))
+    horaEspejo = horaEspejo.replace(hour=horaEspejo.hour - 1)
 print(f"{horaEspejo.hour}:{horaEspejo.minute}")
