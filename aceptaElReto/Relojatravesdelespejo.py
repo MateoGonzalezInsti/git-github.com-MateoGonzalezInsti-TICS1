b1a8f86a184d905a3fb948e0105b5046a91da425
MateoGonzalezInsti <matgonido@iesxulianmagarinos.org>
2024-02-02 12:16:02 +0100

    fiesta_aburrida

--- aceptaElReto/Relojatravesdelespejo.py
+++ aceptaElReto/Relojatravesdelespejo.py
@@ -7,7 +7,7 @@ hora = datetime.datetime.strptime(entrada, "%H:%M").time()
 horaEspejo = hora
 if horaEspejo.hour!=12:
     horaEspejo = horaEspejo.replace(hour=12 - horaEspejo.hour)
-if hora.minute != 0:
+if hora.minute != 0 and hora.minute<30:
     horaEspejo = horaEspejo.replace(minute=(60 - horaEspejo.minute))
     horaEspejo = horaEspejo.replace(hour=horaEspejo.hour - 1)
 print(f"{horaEspejo.hour}:{horaEspejo.minute}")
