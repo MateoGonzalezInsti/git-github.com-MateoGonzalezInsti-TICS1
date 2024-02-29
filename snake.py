a16052e85914e702bbc58b35d48060f050a7e1af
MateoGonzalezInsti <matgonido@iesxulianmagarinos.org>
2024-02-29 13:32:10 +0100

    snake v2

--- snake.py
+++ snake.py
@@ -1,9 +1,17 @@
 import turtle
-#minute video 14:56min; enalce:https://youtu.be/lKzEvbGbbPo
+import time
+import random
+
+window_width=600
+window_height=600
+delay = 0.1
+puntos = 0
+
+# minute video 14:56min; enalce:https://youtu.be/lKzEvbGbbPo
 window = turtle.Screen()  # ventana
 # ajustes ventana
 window.title("Juego Snake Python-By Mateo González")
-window.setup(width=600, height=600)
+window.setup(width=window_width, height=window_height)
 window.bgcolor("light green")
 
 
@@ -15,21 +23,82 @@ head.speed(0)  # velocidad animacion(movimiento)
 head.shape("square")  # forma snake (cubo)
 head.color("green")  # color snake
 head.penup()  # elimina el rastro de la animacion
-head.goto(0, 0)  # posicion (centro ventana)
-head.direction = "up"  # direccion
+head.goto(0, 100)  # posicion (centro ventana)
+head.direction = "stop"  # direccion (up, down, right, left)
+
+# food configuracion
+food = turtle.Turtle()
+food.speed(0)
+food.shape("circle")
+food.color("red")
+food.penup()
+food.goto(0, 0)
+food.direction = "stop"
 
 
 # movimientos del snake
 def mov():
+    oY = head.ycor()  # guardar posicion y
+    oX = head.xcor()  # guardar posicion x
     if head.direction == "up":
-        oY = head.ycor()  # guardar posicion y
-        oX = head.xcor()  # guardar posicion x
-
         head.sety(oY + 10)
+    if head.direction == "down":
+        head.sety(oY - 10)
+    if head.direction == "right":
+        head.setx(oX + 10)
+    if head.direction == "left":
+        head.setx(oX - 10)
+
+
+# teclado movimiento snake
+def dirUp():  # que hacer
+    head.direction = "up"
+
+
+def dirDown():
+    head.direction = "down"
+
+
+def dirRight():
+    head.direction = "right"
+
+
+def dirLeft():
+    head.direction = "left"
+
+
+window.listen()  # escuchando teclado
+window.onkeypress(dirUp, "Up")  # (que hacer,tecla)
+window.onkeypress(dirDown, "Down")
+window.onkeypress(dirRight, "Right")
+window.onkeypress(dirLeft, "Left")
+
+
+# funciones food
+def ColisionFoodSnake():
+    if head.distance(food) < 30:
+        #puntos=1+puntos#sumar puntos
+        Reaparecer()
+
+
+def Reaparecer():
+    minDistance = 30
+    # valor
+    px = random.randint(minDistance, int((window_width / 2) - minDistance))
+    py = random.randint(minDistance, int((window_height / 2) - minDistance))
+    # signo
+    if random.randint(1, 2) == 1:
+        px = -(px)
+    if random.randint(1, 2) == 1:
+        py = -(py)
+    food.goto(px, py)
+
 
 while True:
     window.update()
+    ColisionFoodSnake()
     mov()
+    time.sleep(delay)
 
 
 turtle.done()  # ejecutar codigo
